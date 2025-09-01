90c1a8c3b9737f06b8976294196a8c86448bbc2c
E-Nincy <escotonincy10@outlook.es>
2025-09-01 11:10:41 +0200

    Add json data

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+from django.core.management import call_command
+import os
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+
+    def ready(self):
+        if os.getenv("RENDER") == "1":  # Solo en Render
+            try:
+                from .models import Post
+                if Post.objects.count() == 0:
+                    call_command("loaddata", "blog_data.json")
+            except Exception:
+                pass
+
+
