a040b5ba322d3deb5ccf9f48f552d72b1a7e7c05
E-Nincy <escotonincy10@outlook.es>
2025-09-02 08:06:52 +0200

    change apps and settings

--- blog/apps.py
+++ blog/apps.py
@@ -1,24 +1,5 @@
-# blog/apps.py
 from django.apps import AppConfig
-import os
-import json
-from django.core.management import call_command
-from django.db.utils import OperationalError
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
-
-    def ready(self):
-        if os.getenv("RENDER") == "1":
-            from django.db import connections
-            try:
-                c = connections['default'].cursor()
-                return
-            except OperationalError:
-                json_file = os.path.join(os.path.dirname(__file__), "blog_data.json")
-                if os.path.exists(json_file):
-                    call_command("loaddata", json_file)
-
-
-
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nincy-blog.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,8 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
